5cffce9710a6b68002b1b613e4ea0ba6d40b7ca1
Warren <Warren9555@gmail.com>
2025-04-17 17:00:39 +0200

    model

--- models/User.js
+++ models/User.js
@@ -6,64 +6,19 @@ const userSchema = new mongoose.Schema({
   password: String,
   birthdate: Date,
 
-  gender: { type: String, enum: ["M", "F"] }, // Seuls les valeurs "M" ou "F" sont valides
-  sensitivity: {
-    type: String,
-    enum: [
-      "Frileux",
-      "Sensible au froid",
-      "Neutre",
-      "Le froid ne me dérange pas",
-      "J'ai vite chaud",
-      "Résistant au froid",
-    ],
-  },
-
-  accessories: [
-    {
-      type: String,
-      enum: [
-        "casquette",
-        "bonnet",
-        "écharpe",
-        "gants",
-        "sac à dos",
-        "lunettes de soleil",
-        "aucun",
-      ], // Accessoires favoris
-    },
-  ],
-  recommendationFrequency: {
-    type: String,
-    enum: [
-      "Une tenue par jour",
-      "Une tenue seulement si la météo change beaucoup",
-      "Plusieurs suggestions pour choisir",
-    ],
-  }, // Correction : fermeture correcte de l'objet
+  gender: String, // Suppression de l'enum pour le genre
+  sensitivity: String, // Suppression de l'enum pour la sensibilité
+  accessories: [String], // Suppression de l'enum pour les accessoires
+  recommendationFrequency: String, // Suppression de l'enum pour la fréquence des recommandations
 
   preferencesCompleted: { type: Boolean, default: false },
   children: [{ type: mongoose.Schema.Types.ObjectId, ref: "Child" }], // Référence aux enfants
 
-  // Champs pour la réinitialisation du mot de passe
-  resetToken: {
-    type: String,
-    default: null,
-  },
-  resetTokenExpiry: {
-    type: Date,
-    default: null,
-  },
   expoPushToken: {
     type: String,
     default: null,
   },
-  notificationTimes: [
-    {
-      type: String,
-      enum: ["morning", "noon", "evening"],
-    },
-  ],
+  notificationTimes: [String], // Suppression de l'enum pour les heures de notification
   notificationsEnabled: {
     type: Boolean,
     default: false,
